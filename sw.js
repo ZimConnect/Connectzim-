cea82d04a69c8cc3b776e161dbde19dc6008405a
ZimConnect <berryzcarwash@gmail.com>
2026-08-08 07:53:36 +0200

    Add service worker for caching files

---
+++ sw.js
@@ -0,0 +1,9 @@
+const CACHE = "zimconnect-v3";
+const FILES = ["/","/index.html","/manifest.json","/icon-512.png","/icon-192.png"];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
