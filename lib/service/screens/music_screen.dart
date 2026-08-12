eb42c9153e2085e10ee6b96da3d1566dd8cd438c
ZimConnect <berryzcarwash@gmail.com>
2026-08-12 18:35:40 +0200

    Add music and radio playback features
    Implemented music and radio loading functionality in MusicScreen.

--- lib/service/screens/music_screen.dart
+++ lib/service/screens/music_screen.dart
@@ -1,1 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import '../services/api_service.dart';
 
+class MusicScreen extends StatefulWidget {
+  @override
+  _MusicScreenState createState() => _MusicScreenState();
+}
+
+class _MusicScreenState extends State<MusicScreen> {
+  List songs = [];
+  List radio = [];
+  final player = AudioPlayer();
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    songs = await ApiService.getMusic();
+    radio = await ApiService.getRadio(); // ZimConnect Radio 263 - DJ Tewe
+    setState(() {});
+  }
+
+  play(String url) {
+    player.setUrl(url);
+    player.play();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.green[100],
+          padding: EdgeInsets.all(12),
+          child: Column(children: [
+            Text("ZimConnect Radio 263 - Hosted by DJ Tewe", 
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
+          ...radio.map((r) => ListTile(
+              leading: Icon(Icons.radio, color: Colors.red),
+              title: Text(r['name']),
+              subtitle: Text("LIVE"),
+              onTap: () => play(r['streamUrl']),
+            )),
+          ]),
+        ),
+        Divider(),
+        Text("Music Library", style: TextStyle(fontWeight: FontWeight.bold)),
+        Expanded(child: songs.isEmpty 
+      ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+          itemCount: songs.length,
+          itemBuilder: (context, i) => ListTile(
+            leading: Icon(Icons.music_note),
+            title: Text(songs[i]['title']),
+            subtitle: Text(songs[i]['artist']),
+            onTap: () => play(songs[i]['musicUrl']),
+          ),
+        ))
+      ],
+    );
+  }
+}
